64d30376e2f9e5593598bbd1245eb16f0dd442b4
MedABH <128604732+MedABH@users.noreply.github.com>
2025-01-27 16:35:01 +0100

    27/1/2025

---
+++ servicesHTML/fmc.js
@@ -0,0 +1,20 @@
+/* ---- Image bannner stop mouse hover ---- */
+/*document.addEventListener("DOMContentLoaded", () => {
+    const carouselInner = document.querySelector('#carousel-inner');
+  
+    // Ensure the carousel exists in the DOM
+    if (carouselInner) {
+      // Pause the animation when the user hovers over the carousel
+      carouselInner.addEventListener('mouseenter', () => {
+        carouselInner.style.animationPlayState = 'paused'; // Pause animation
+      });
+  
+      // Resume the animation when the mouse leaves the carousel
+      carouselInner.addEventListener('mouseleave', () => {
+        carouselInner.style.animationPlayState = 'running'; // Resume animation
+      });
+    } else {
+      console.error("Carousel inner container not found. Check your HTML structure.");
+    }
+  });*/
+
