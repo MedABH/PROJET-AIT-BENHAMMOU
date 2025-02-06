fdd5cb62d6283bfaf9d5c8ce02256d1c167213db
MedABH <128604732+MedABH@users.noreply.github.com>
2025-02-06 17:11:51 +0100

    fin de minimiser les liens

---
+++ js/js.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.getElementById("imgSliderCon");
+    const slides = slider.children;
+    const sliderDots = document.querySelectorAll(".sliderPosition");
+    let currentIndex = 0;
+    const slideInterval = 5000; // Durée de chaque slide en millisecondes
+    const transitionDuration = 900; // Durée de la transition en millisecondes
+
+    function changeSlide(nextIndex) {
+        // Mise à jour de l'index actuel
+        const totalSlides = slides.length;
+        currentIndex = (nextIndex + totalSlides) % totalSlides;
+
+        // Déplacement du slider
+        slider.style.transition = `${transitionDuration}ms ease-out`;
+        slider.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+        // Mise à jour des couleurs des points
+        sliderDots.forEach((dot, index) => {
+            if (index === currentIndex) {
+                dot.style.backgroundColor = "#4f6fce"; // Bleu pour le point actif
+            } else {
+                dot.style.backgroundColor = "#ffffff"; // Blanc pour les autres points
+            }
+        });
+    }
+    // Ajouter les événements de clic sur les points
+    sliderDots.forEach((dot, index) => {
+        dot.addEventListener("click", () => changeSlide(index));
+    });
+    // Configurer le changement automatique de slide
+    setInterval(() => {
+        changeSlide(currentIndex + 1);
+    }, slideInterval);
+});
+
+/* ---- Image bannner stop mouse hover ---- */
+document.addEventListener("DOMContentLoaded", () => {
+    const carouselInner = document.querySelector('#carousel-inner-blog');
+  
+    // Ensure the carousel exists in the DOM
+    if (carouselInner) {
+      // Pause the animation when the user hovers over the carousel
+      carouselInner.addEventListener('mouseenter', () => {
+        carouselInner.style.animationPlayState = 'paused'; // Pause animation
+      });
+  
+      // Resume the animation when the mouse leaves the carousel
+      carouselInner.addEventListener('mouseleave', () => {
+        carouselInner.style.animationPlayState = 'running'; // Resume animation
+      });
+    } else {
+      console.error("Carousel inner container not found. Check your HTML structure.");
+    }
+  });
